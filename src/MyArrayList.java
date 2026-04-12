7ed3d167fdd9d2643417b49ef4f689f800749707
Shera <sherhan.amangeldin@icloud.com>
2026-04-12 19:24:49 -0400

    implement dynamic resizing and basic add method

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -17,5 +17,19 @@ public class MyArrayList<T> implements MyList<T> {
     public boolean exists(Object object) {
         return indexOf(object) != -1;
     }
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            for (int i = 0; i < size; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+    }
 
+    @Override
+    public void add(T item) {
+        ensureCapacity();
+        elements[size++] = item; // [cite: 8]
+    }
 }
